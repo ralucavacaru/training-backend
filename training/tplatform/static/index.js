6c82883e03d1e8f7f16e4a65e646cc11c1a4eb01
ralucavacaru <raluca_vacaru@yahoo.com>
2017-07-30 16:49:46 -0400

    Commit before changing a lot of structural stuff
    FINGERS CROSSED

--- training/tplatform/static/index.js
+++ training/tplatform/static/index.js
@@ -81,3 +81,21 @@ $(document).ready(function() {
 		}
 	})
 });
+
+function footerAlign() {
+  $('footer').css('display', 'block');
+  $('footer').css('height', 'auto');
+  var footerHeight = $('footer').outerHeight();
+  $('body').css('padding-bottom', footerHeight);
+  $('footer').css('height', footerHeight);
+}
+
+
+$(document).ready(function(){
+  footerAlign();
+});
+
+$( window ).resize(function() {
+  footerAlign();
+});
+
