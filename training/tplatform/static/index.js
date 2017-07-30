a99e54e7f72e669b9910be756ed2e689082a88c2
ralucavacaru <raluca_vacaru@yahoo.com>
2017-07-30 17:32:43 -0400

    FOOTER WORKS OMG

--- training/tplatform/static/index.js
+++ training/tplatform/static/index.js
@@ -82,20 +82,3 @@ $(document).ready(function() {
 	})
 });
 
-function footerAlign() {
-  $('footer').css('display', 'block');
-  $('footer').css('height', 'auto');
-  var footerHeight = $('footer').outerHeight();
-  $('body').css('padding-bottom', footerHeight);
-  $('footer').css('height', footerHeight);
-}
-
-
-$(document).ready(function(){
-  footerAlign();
-});
-
-$( window ).resize(function() {
-  footerAlign();
-});
-
